8736bcb3046b5d784c0e56fb7e56a3bf516efa75
Alan Sanchez <me@alansanchez.dev>
2023-06-19 02:49:49 -0700

    Init repo

---
+++ database/migrations/2023_06_10_214643_add_columns_to_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('username')->unique();
+            $table->foreignId('profile_id')->constrained();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['profile_id']);
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->removeColumn('username');
+            $table->removeColumn('profile_id');
+        });
+    }
+};
